779cbeea64c682c1a95129ecef8a23638dda8d01
Nimosana <Nicolas.morales39@yahoo.fr>
2023-09-16 23:03:35 -0400

    Less epileptic

--- exercices/i-like-to-move-it/js/script.js
+++ exercices/i-like-to-move-it/js/script.js
@@ -19,13 +19,15 @@ let maxCanvasSize = 600;
 let minCanvasSize = 500;
 let direction = 1;
 let direction2 = 1;
+let direction3 = 1;
 let offset = 0;
 let offset2 = 0;
+let offset3 = 0;
 let randomColorHead = 0;
 let randomColorNostrils = 0;
-let randomColorR = 0;
-let randomColorG = 0;
-let randomColorB = 0;
+let randomColorR1 = 0;
+let randomColorG1 = 0;
+let randomColorB1 = 0;
 let randomColorR2 = 0;
 let randomColorG2 = 0;
 let randomColorB2 = 0;
@@ -66,34 +68,37 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    //resizeCanvas(canvasWidth, canvasHeight);
     x1 = constrain((map(mouseX, 0, canvasWidth, 0, canvasWidth, true)) - (canvasWidth / 2), -(canvasHeight * 0.25), (canvasHeight * 0.25));
     y1 = constrain((map(mouseY, 0, canvasWidth, 0, canvasWidth, true)) - ((canvasWidth / 2) + 140), -(canvasHeight * 0.5), 0);
     randomColors();
-    boppingX(0, 10)
-    boppingY(0, 30)
+    boppingX(0, 10);
+    boppingY(0, 30);
+    bodyWiggle(20);
     background(0, 0, 0);
     //Alien
     noStroke();
-    //rect(0, canvasWidth / 2, canvasWidth / 2);
-    fill(randomColorR, randomColorG, randomColorB);
-    //head
+    fill(randomColorR1, randomColorG1, randomColorB1);
+    //head top
     ellipse((canvasHeight / 2) + XOffset + x1, ((canvasWidth / 2) + canvasWidth / 8) + YOffset + y1, canvasHeight / 2, canvasHeight / 4);
     fill(0);
     rect(0 + x1, canvasWidth * (5 / 8) + YOffset + y1, canvasWidth, canvasHeight);
-    fill(randomColorR, randomColorG, randomColorB);
+    fill(randomColorR1, randomColorG1, randomColorB1);
+    //body
+    ellipse(((canvasHeight / 2) + XOffset + x1), canvasWidth + YOffset + y1 + 180, (canvasHeight / 4) + offset3, canvasWidth + offset3);
+    //head bottom
     triangle(canvasWidth / 4 + XOffset + x1, (canvasWidth * (5 / 8) + YOffset + y1), (canvasWidth * 0.75) + XOffset + x1, (canvasWidth * (5 / 8) + YOffset + y1), (canvasWidth / 2) + x1, canvasHeight + YOffset + y1);
     //mouth
     fill(randomColorR2, randomColorG2, randomColorB2);
     ellipse((canvasWidth / 2) + XOffset + x1, (canvasWidth * 3 / 4) + YOffset + y1, canvasHeight / 8, canvasHeight / 4);
-    fill(randomColorR, randomColorG, randomColorB);
+    fill(randomColorR1, randomColorG1, randomColorB1);
     ellipse((canvasWidth / 2) + XOffset + x1, ((canvasWidth * 3 / 4) - 10) + YOffset + y1, canvasHeight / 7, canvasHeight / 4);
-    //eyes
+    //left eye
     fill(randomColorR3, randomColorG3, randomColorB3);
     translate((canvasWidth * (6 / 16) - 8) + XOffset + x1, (canvasWidth * (10.6 / 16)) + YOffset + y1);
     rotate(PI / 7);
     ellipse(0, 0, 120, 40);
     resetMatrix();
+    //right eye
     translate((canvasWidth * (10 / 16) + 8) + XOffset + x1, canvasWidth * ((10.6 / 16)) + YOffset + y1);
     rotate(-(PI / 7));
     fill(randomColorR4, randomColorG4, randomColorB4);
@@ -109,24 +114,28 @@ function draw() {
 function randomColors() {
     randomColorHead = (Math.random() * 50 + 0);
     randomColorNostrils = (Math.random() * 50);
-    randomColorR = (Math.random() * 255);
-    randomColorG = (Math.random() * 255);
-    randomColorB = (Math.random() * 255);
-    randomColorR2 = (Math.random() * 255);
-    randomColorG2 = (Math.random() * 255);
-    randomColorB2 = (Math.random() * 255);
+    //body & head
+    randomColorR1 = (Math.random() * 80);
+    randomColorG1 = (Math.random() * 80);
+    randomColorB1 = (Math.random() * 80);
+    //mouth
+    randomColorR2 = (Math.random() * 160);
+    randomColorG2 = (Math.random() * 160);
+    randomColorB2 = (Math.random() * 160);
+    //eyes
     randomColorR3 = (Math.random() * 255);
     randomColorG3 = (Math.random() * 255);
     randomColorB3 = (Math.random() * 255);
     randomColorR4 = (Math.random() * 255);
     randomColorG4 = (Math.random() * 255);
     randomColorB4 = (Math.random() * 255);
-    randomColorR5 = (Math.random() * 255);
-    randomColorG5 = (Math.random() * 255);
-    randomColorB5 = (Math.random() * 255);
-    randomColorR6 = (Math.random() * 255);
-    randomColorG6 = (Math.random() * 255);
-    randomColorB6 = (Math.random() * 255);
+    //nostrils
+    randomColorR5 = (Math.random() * 120);
+    randomColorG5 = (Math.random() * 120);
+    randomColorB5 = (Math.random() * 120);
+    randomColorR6 = (Math.random() * 120);
+    randomColorG6 = (Math.random() * 120);
+    randomColorB6 = (Math.random() * 120);
 }
 
 function boppingX(originalPos, maxOffset) {
@@ -142,4 +151,10 @@ function boppingY(originalPos, maxOffset) {
         direction2 *= -1;
     }
     YOffset = originalPos + offset2;
+}
+function bodyWiggle(maxOffset) {
+    offset3 += direction3 * 1;
+    if (offset3 >= maxOffset || offset3 <= -maxOffset) {
+        direction3 *= -1;
+    }
 }
