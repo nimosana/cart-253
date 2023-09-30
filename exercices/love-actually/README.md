2d88870aed92d6f8d6b4913fb64bee9c09d6a92d
Nimosana <Nicolas.morales39@yahoo.fr>
2023-09-30 00:10:50 -0400

    Ex 3: Started Love, Actually exercise

---
+++ exercices/love-actually/README.md
@@ -0,0 +1,4 @@
+# Love, actually
+## Nicolas Morales-Sanabria
+
+This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
---
+++ exercices/love-actually/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Exercise 3: Love, Actually
+ * @author Nicolas Morales-Sanabria
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
