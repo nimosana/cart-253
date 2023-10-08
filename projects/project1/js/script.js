f5ba1932045ddb2f3785520745f6ad95d19c763f
Nimosana <Nicolas.morales39@yahoo.fr>
2023-10-08 14:34:46 -0400

    P1: upgraded user keyMovement
    I imported the keyMovement function I made for the love-actually activity, it now works for ArrowKeys & WASD and takes into account the total speed in x & y (pythagorean theorem) to correctly limit to the max speed

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,6 +7,20 @@
  */
 
 "use strict";
+let user = {
+    x: undefined,
+    y: undefined,
+    vx: 0,
+    vy: 0,
+    size: 100,
+    speed: 1,
+    maxSpeed: 5,
+    directionX: 1,
+    directionY: 1,
+    accelX: 0.5,
+    accelY: 0.5,
+    texture: null
+}
 
 /**
  * Description of preload
@@ -20,7 +34,9 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    user.x = windowWidth / 2;
+    user.y = windowHeight / 2;
 }
 
 
@@ -28,5 +44,43 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+    keyMovement(user);
+    ellipse(user.x, user.y, user.size);
+}
 
+
+/** Allows the user to control an object's speed with accelerations, using the arrow keys
+ * @param obj object to be controlled using the arrow keys or WASD */
+function keyMovement(obj) {
+    //horizontal movement
+    if ((keyIsDown(39) && !keyIsDown(37)) || (keyIsDown(68) && !keyIsDown(65))) {
+        obj.vx = obj.vx + obj.accelX;
+    }
+    else if (keyIsDown(37) && !keyIsDown(39) || (keyIsDown(65) && !keyIsDown(68))) {
+        obj.vx = obj.vx - obj.accelX;
+    }
+    else if ((!keyIsDown(37) && !keyIsDown(39)) || (keyIsDown(37) && keyIsDown(39)) || (keyIsDown(65) && keyIsDown(68))) {
+        obj.vx /= 1.03;
+    }
+    //vertical movement
+    if ((keyIsDown(38) && !keyIsDown(40)) || (keyIsDown(87) && !keyIsDown(83))) {
+        obj.vy = obj.vy - obj.accelY;
+    }
+    else if (keyIsDown(40) && !keyIsDown(38) || (keyIsDown(83) && !keyIsDown(87))) {
+        obj.vy = obj.vy + obj.accelY;
+    }
+    else if ((!keyIsDown(40) && !keyIsDown(38)) || (keyIsDown(40) && keyIsDown(38)) || (keyIsDown(83) && keyIsDown(87))) {
+        obj.vy /= 1.03;
+    }
+    //limit to max speed
+    let speed = sqrt(pow(obj.vx, 2) + pow(obj.vy, 2));
+    if (speed > obj.maxSpeed) {
+        obj.vx *= (obj.maxSpeed / speed);
+        obj.vy *= (obj.maxSpeed / speed);
+    }
+    //move obj
+    obj.x += obj.vx;
+    obj.y += obj.vy;
+    console.log(`spd X: ${obj.vx} spd Y: ${obj.vy}`)
 }
