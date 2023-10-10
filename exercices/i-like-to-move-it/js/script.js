256791ee345d045612f3ff170aacc2673c111b26
Nimosana <Nicolas.morales39@yahoo.fr>
2023-10-10 02:03:13 -0400

    E1: generalizing alien code
    Simplifying the alien's code and generalizing its structure so I can adapt it into a class

--- exercices/i-like-to-move-it/js/script.js
+++ exercices/i-like-to-move-it/js/script.js
@@ -7,46 +7,33 @@
  * My submission for Excercise 1: I like to move it!
  */
 "use strict";
-var canvasWidth = 500;
-var canvasHeight = canvasWidth;
-let direction = 1;
-let direction2 = 1;
-let direction3 = 1;
-let randomColorR1 = 0;
-let randomColorG1 = 0;
-let randomColorB1 = 0;
-let randomColorR2 = 0;
-let randomColorG2 = 0;
-let randomColorB2 = 0;
-let randomColorR3 = 0;
-let randomColorG3 = 0;
-let randomColorB3 = 0;
-let randomColorR4 = 0;
-let randomColorG4 = 0;
-let randomColorB4 = 0;
-let randomColorR5 = 0;
-let randomColorG5 = 0;
-let randomColorB5 = 0;
-let randomColorR6 = 0;
-let randomColorG6 = 0;
-let randomColorB6 = 0;
-let offset = 0;
-let offset2 = 0;
-let offset3 = 0;
-let xOffset = 0;
-let yOffset = 0;
-let x1 = 0;
-let y1 = 0;
-let horizontalMovement = 0;
-let verticalMovement = 0;
+let size;
+let randomColorR1, randomColorG1, randomColorB1;
+let randomColorR2, randomColorG2, randomColorB2;
+let randomColorR3, randomColorG3, randomColorB3;
+let randomColorR4, randomColorG4, randomColorB4;
+let randomColorR5, randomColorG5, randomColorB5;
+let randomColorR6, randomColorG6, randomColorB6;
+let direction, direction2, direction3, direction4;
+let offset, offset2, offset3, offset4;
+let xOffset, yOffset;
+let x1, y1;
+let horizontalMovement, verticalMovement;
 
 /**
  * Creates the canvas and sets the text size used in the animation.
 */
 function setup() {
-    createCanvas(canvasHeight, canvasHeight);
-    textSize(20);
-    strokeWeight(20)
+    direction = direction2 = direction3 = direction4 = 1;
+    offset = offset2 = offset3 = offset4 = 0;
+    xOffset = yOffset = 0;
+    x1 = y1 = 0;
+    horizontalMovement = verticalMovement = 0;
+    size = windowHeight;
+    createCanvas(size, size);
+    textSize(size * 0.04);
+    strokeWeight(size * 0.04)
+    textAlign(CENTER, CENTER)
 }
 
 /**
@@ -57,66 +44,71 @@ function draw() {
     background(0, 0, 0);
     //x1 and y1 values determine a position that follows the mouse,
     //the constrain on "map" to keeps the alien's whole head in the canvas
-    x1 = constrain(map(mouseX, 0, canvasWidth, 0, canvasWidth) - (canvasWidth / 2), -(canvasHeight * 0.25), (canvasHeight * 0.25));
-    y1 = constrain(map(mouseY, 0, canvasHeight, 0, canvasHeight) - ((canvasHeight / 2) + 140), -(canvasHeight * 0.5), 0);
+    // x1 = constrain(map(mouseX, 0, size, 0, size) - (size / 2), -(size * 0.25), (size * 0.25));
+    // y1 = constrain(map(mouseY, 0, size, 0, size) - ((size / 2) + size * 0.28), -(size * 0.5), 0);
     //the code without constrain would let the alien follow the cursor up to the corner of the canvas (with map's withinBounds =true)
-    //x1 = map(mouseX, 0, canvasWidth, 0, canvasWidth, true) - (canvasWidth / 2), -(canvasHeight * 0.25), (canvasHeight * 0.25);
-    //y1 = map(mouseY, 0, canvasHeight, 0, canvasHeight, true) - ((canvasHeight / 2) + 140), -(canvasHeight * 0.5), 0;
-
+    //x1 = map(mouseX, 0, size, 0, size, true) - (size / 2), -(size * 0.25), (size * 0.25);
+    // y1 = map(mouseY, 0, size, 0, size, true) - ((size / 2) + 140), -(size * 0.5), 0;
+    y1 = -(size / 3);
     //randomize colors and determine next movement
     randomColors();
-    boppingX(10);
-    boppingY(30);
-    bodyWiggle(20);
+    boppingX(size * 0.02);
+    boppingY(size * 0.06);
+    bodyWiggle(size * 0.04);
+    eyesClosing();
     horizontalMovement = xOffset + x1;
     verticalMovement = yOffset + y1;
     //The following code draws the alien
     //top of the head
     noStroke();
     fill(randomColorR1, randomColorG1, randomColorB1);
-    ellipse((canvasWidth / 2) + horizontalMovement, (canvasHeight / 2) + (canvasHeight / 8) + verticalMovement, canvasWidth / 2, canvasHeight / 4);
+    ellipse((size / 2) + horizontalMovement, (size / 2) + (size / 8) + verticalMovement, size / 2, size / 4);
     fill(0);
-    rect(0 + x1, canvasHeight * (5 / 8) + verticalMovement, canvasWidth, canvasHeight);
-    //body
+    rect(0 + x1, size * (5 / 8) + verticalMovement, size, size);
     fill(randomColorR1, randomColorG1, randomColorB1);
     stroke(randomColorR1, randomColorG1, randomColorB1);
     //left arm
-    line(canvasWidth / 2 + horizontalMovement, canvasHeight + verticalMovement, (canvasWidth / 8) + horizontalMovement, canvasHeight - (yOffset / 5) + y1 + 50);
-    line((canvasWidth / 8) + horizontalMovement, canvasHeight - (yOffset / 5) + y1 + 50, (canvasWidth * 0.2) + horizontalMovement, canvasHeight - yOffset * 5 + y1 + -50);
+    line(size / 2 + horizontalMovement, size + verticalMovement, (size / 8) + horizontalMovement, size - (yOffset / 5) + y1 + size * 0.1);
+    line((size / 8) + horizontalMovement, size - (yOffset / 5) + y1 + size * 0.1, (size * 0.2) + horizontalMovement, size - yOffset * 5 + y1 + -size * 0.1);
     //right arm
-    line(canvasWidth / 2 + horizontalMovement, canvasHeight + verticalMovement, (canvasWidth * 0.875) + horizontalMovement, canvasHeight - (yOffset / 5) + y1 + 50);
-    line((canvasWidth * 0.875) + horizontalMovement, canvasHeight - (yOffset / 5) + y1 + 50, (canvasWidth * 0.8) + horizontalMovement, canvasHeight + yOffset * 5 + y1 + -50);
+    line(size / 2 + horizontalMovement, size + verticalMovement, (size * 0.875) + horizontalMovement, size - (yOffset / 5) + y1 + size * 0.1);
+    line((size * 0.875) + horizontalMovement, size - (yOffset / 5) + y1 + size * 0.1, (size * 0.8) + horizontalMovement, size + yOffset * 5 + y1 + -size * 0.1);
     noStroke();
-    ellipse((canvasWidth / 2) + horizontalMovement, canvasHeight + verticalMovement + 180, (canvasWidth / 4) + offset3, canvasHeight + offset3);
+    //body
+    ellipse((size / 2) + horizontalMovement, size + verticalMovement + size * 0.36, (size / 4) + offset3, size + offset3);
+    fill(0);
+    rect((size / 2) + horizontalMovement - (size / 4), size + verticalMovement + size * 0.6, (size / 2))
     //CLASSIFIED
     fill(randomColorR2, randomColorG2, randomColorB2);
-    text("BELIEVE", (canvasWidth / 2) + horizontalMovement - 40, (canvasHeight / 2) + verticalMovement + 375);
+    text("BELIEVE", (size / 2) + horizontalMovement, (size / 2) + verticalMovement + size * 0.75);
     //bottom of the head
     fill(randomColorR1, randomColorG1, randomColorB1);
-    triangle(canvasWidth / 4 + horizontalMovement, canvasHeight * (5 / 8) + verticalMovement, (canvasWidth * 0.75) + horizontalMovement, canvasWidth * (5 / 8) + verticalMovement,
-        (canvasWidth / 2) + x1, canvasHeight + verticalMovement);
+    triangle(size / 4 + horizontalMovement, size * (5 / 8) + verticalMovement, (size * 0.75) + horizontalMovement, size * (5 / 8) + verticalMovement,
+        (size / 2) + x1, size + verticalMovement);
     //mouth
     fill(randomColorR2, randomColorG2, randomColorB2);
-    ellipse((canvasWidth / 2) + horizontalMovement, (canvasHeight * 3 / 4) + verticalMovement, canvasWidth / 8, canvasHeight / 4);
+    ellipse((size / 2) + horizontalMovement, (size * 3 / 4) + verticalMovement, size / 8, size / 4);
     fill(randomColorR1, randomColorG1, randomColorB1);
-    ellipse((canvasWidth / 2) + horizontalMovement, (canvasHeight * 3 / 4) - 10 + verticalMovement, canvasWidth / 7, canvasHeight / 4);
+    ellipse((size / 2) + horizontalMovement, (size * 3 / 4) - size * 0.02 + verticalMovement, size / 7, size / 4);
     //left eye
+    push();
     fill(randomColorR3, randomColorG3, randomColorB3);
-    translate(canvasWidth * (6 / 16) - 8 + horizontalMovement, canvasHeight * (10.6 / 16) + verticalMovement);
+    translate(size * (6 / 16) - size * 0.016 + horizontalMovement, size * (10.6 / 16) + verticalMovement);
     rotate(PI / 7);
-    ellipse(0, 0, 120, 20 + offset3);
-    resetMatrix();
+    ellipse(0, 0, size * 0.24, size * 0.04 + offset4);
+    pop();
     //right eye
-    translate(canvasWidth * (10 / 16) + 8 + horizontalMovement, canvasHeight * (10.6 / 16) + verticalMovement);
+    push();
+    translate(size * (10 / 16) + size * 0.016 + horizontalMovement, size * (10.6 / 16) + verticalMovement);
     rotate(-(PI / 7));
     fill(randomColorR4, randomColorG4, randomColorB4);
-    ellipse(0, 0, 120, 20 + offset3);
-    resetMatrix();
+    ellipse(0, 0, size * 0.24, size * 0.04 + offset4);
+    pop();
     //nostrils
     fill(randomColorR5, randomColorG5, randomColorB5);
-    ellipse(canvasWidth * (8 / 16) - 10 + horizontalMovement, (canvasHeight / 1.5) + 50 + verticalMovement, 5, 5);
+    ellipse(size * (8 / 16) - size * 0.02 + horizontalMovement, (size / 1.5) + size * 0.1 + verticalMovement, size * 0.01, size * 0.01);
     fill(randomColorR6, randomColorG6, randomColorB6);
-    ellipse(canvasWidth * (8 / 16) + 10 + horizontalMovement, (canvasHeight / 1.5) + 50 + verticalMovement, 5, 5);
+    ellipse(size * (8 / 16) + size * 0.02 + horizontalMovement, (size / 1.5) + size * 0.1 + verticalMovement, size * 0.01, size * 0.01);
 }
 /**
  * Randomizes the RGB values for the different colors used
@@ -151,7 +143,7 @@ function randomColors() {
  * @param maxOffset maximum value of the offset (absolute)
 */
 function boppingX(maxOffset) {
-    offset += direction * 1;
+    offset += direction * size * 2.1E-3;
     if (offset >= maxOffset || offset <= -maxOffset) {
         direction *= -1;
     }
@@ -162,18 +154,28 @@ function boppingX(maxOffset) {
  * @param maxOffset maximum value of the offset (absolute)
 */
 function boppingY(maxOffset) {
-    offset2 += direction2 * 2;
+    offset2 += direction2 * size * 4.2E-3;
     if (offset2 >= maxOffset || offset2 <= -maxOffset) {
         direction2 *= -1;
     }
     yOffset = offset2;
 }
 /**
+ * generates the open and closing movement of the eyes
+ */
+function eyesClosing() {
+    let maxOffset = size * 0.04;
+    offset4 += direction4 * (size * 0.04) / 30;
+    if (offset4 >= maxOffset || offset4 <= -maxOffset) {
+        direction4 *= -1;
+    }
+}
+/**
  * modifies offset3, the value oscillates between its max offset
  * @param maxOffset maximum value of the offset (absolute)
 */
 function bodyWiggle(maxOffset) {
-    offset3 += direction3 * 1;
+    offset3 += direction3 * 2E-3;
     if (offset3 >= maxOffset || offset3 <= -maxOffset) {
         direction3 *= -1;
     }
