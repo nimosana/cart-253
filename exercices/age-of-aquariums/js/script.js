dd8d4d0971933b16f046da082deea4f6e75cc866
Nimosana <Nicolas.morales39@yahoo.fr>
2023-10-23 02:34:54 -0400

    Ex 4: Completed aquarium activity

--- exercices/age-of-aquariums/js/script.js
+++ exercices/age-of-aquariums/js/script.js
@@ -7,6 +7,8 @@
  */
 
 "use strict";
+let school = [];
+let schoolSize = 10;
 
 /**
  * Description of preload
@@ -15,18 +17,66 @@ function preload() {
 
 }
 
-
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(600, 600);
+    for (let i = 0; i < schoolSize; i++) {
+        let fish = createFish(random(0, width), random(0, height))
+        school.push(fish);
+    }
 }
-
 
 /**
  * Description of draw()
 */
 function draw() {
+    background(0);
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
+}
 
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
+}
+
+function moveFish(fish) {
+    // Choose whether to change direction
+    let change = random(0, 1);
+    if (change < 0.05) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
+    
+    // Move the fish
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+
+    // Constrain the fish to the canvas
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+function displayFish(fish) {
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
+}
+
+function mousePressed() {
+    let fish = createFish(mouseX, mouseY);
+    school.push(fish);
 }
